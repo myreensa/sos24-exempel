03ee4a46ec090941bd8ea7671ffc18132f4e46d6
myreensa <myreensa@arcada.fi>
2024-03-25 15:40:20 +0200

    medelupdate

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    // 1. Deklarera objektet.
+    EditText inputText;
+
+    double[] values = { 3.5, 4.4, 9.0, 1.2, 6.5, 12.0 }; // code challenge
+
+    outputText.SetText(String.format((
+            "medelvärde: %.2f"
+            Statistics.calcMean(values)));
+}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputText = findViewById(R.id.outText); // 2. initialisera ett värde
+        inputText = findViewById(R.id.inputText);
+
+
+      ; // 3. köra metoder på objektet
+    }
+    public void buttonHandler(View view) {
+        outputText.setText(inputText.getText().toString()); // setters and getters
+    }
+    public void calculate(View view) {
+        double sum = 0.0;
+        for(int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        double mean = sum / values.length; // code challenge
+        outputText.setText(String.format("Medelvärde: %.2f", mean));
+
 
     }
 }
